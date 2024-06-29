2001c667de5f7634ad4c824943920a10c7b73b40
captainPijay <muhammadzahran02@gmail.com>
2024-06-29 14:51:50 +0700

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+}
+
+function hideMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.remove("active");
+}
